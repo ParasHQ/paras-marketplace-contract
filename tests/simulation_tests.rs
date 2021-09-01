71ea7b401b81435a8cbc96f674919738a2c8f534
Irfianto <irfi@paras.id>
2021-09-01 15:15:40 +0700

    test: add simulation test

--- tests/simulation_tests.rs
+++ tests/simulation_tests.rs
@@ -1,13 +1,96 @@
 use near_sdk::AccountId;
-use near_sdk_sim::{view};
+use near_sdk::serde_json::json;
+use near_sdk_sim::{view, call, to_yocto, DEFAULT_GAS};
 
-use crate::utils::{init};
+use crate::utils::{init, create_nft_and_mint_one, STORAGE_ADD_MARKET_DATA};
 mod utils;
 
 #[test]
 fn test_new() {
-    let (marketplace, nft, treasury, alice, bob) = init();
+    let (marketplace, _, treasury, _, _, _, _) = init();
 
     let treasury_id: AccountId = view!(marketplace.get_treasury()).unwrap_json();
     assert_eq!(treasury_id, treasury.account_id());
+}
+
+#[test]
+fn test_add_market_data() {
+    let (marketplace, nft, _, alice, bob, chandra, _) = init();
+
+    //owner marketplace and nft-> alice
+    //seller -> bob
+    //buyer -> chandra
+    //treasury -> treasury
+    //royalty to 10 different account
+
+    create_nft_and_mint_one(&nft, &alice, &bob, &chandra);
+    let msg = &json!({"price": to_yocto("3").to_string(), "ft_token_id": "near"}).to_string();
+
+    let initial_storage_usage = marketplace.account().unwrap().storage_usage;
+
+    let outcome = chandra.call(
+        nft.account_id(),
+        "nft_approve",
+        &json!({
+            "token_id": format!("{}:{}", u128::MAX.to_string(), "1"),
+            "account_id": marketplace.valid_account_id(),
+            "msg": msg,
+        }).to_string().into_bytes(),
+        DEFAULT_GAS,
+        STORAGE_ADD_MARKET_DATA,
+    );
+
+    outcome.assert_success();
+    let storage_price_for_add_market = 
+        (marketplace.account().unwrap().storage_usage - initial_storage_usage) as u128 * 10u128.pow(19);
+    println!("{:?}", outcome.promise_results());
+    println!("[ADD MARKET DATA] Gas burnt: {} TeraGas", outcome.gas_burnt() as f64 / 1e12);
+    println!("[ADD MARKET DATA] Storage price : {} yoctoNEAR", storage_price_for_add_market);
+}
+
+#[test]
+fn test_buy() {
+    let (marketplace, nft, treasury, alice, bob, chandra, root) = init();
+
+
+    create_nft_and_mint_one(&nft, &alice, &bob, &chandra);
+    let msg = &json!({"price": to_yocto("3").to_string(), "ft_token_id": "near"}).to_string();
+
+    chandra.call(
+        nft.account_id(),
+        "nft_approve",
+        &json!({
+            "token_id": format!("{}:{}", u128::MAX.to_string(), "1"),
+            "account_id": marketplace.valid_account_id(),
+            "msg": msg,
+        }).to_string().into_bytes(),
+        DEFAULT_GAS,
+        STORAGE_ADD_MARKET_DATA,
+    ).assert_success();
+
+    //buyer
+    let buyer_person = root.create_user(
+        "o".repeat(64),
+        to_yocto("100")
+    );
+
+    let initial_storage_usage = marketplace.account().unwrap().storage_usage;
+
+    let outcome =  buyer_person.call(
+        marketplace.account_id(),
+        "buy",
+        &json!({
+            "nft_contract_id": nft.account_id(),
+            "token_id": format!("{}:{}", u128::MAX.to_string(), "1"),
+        }).to_string().into_bytes(),
+        DEFAULT_GAS,
+        to_yocto("3"),
+    );
+
+    let restored_storage_price_for_buy = 
+        initial_storage_usage - marketplace.account().unwrap().storage_usage;
+
+    println!("{:?}", outcome.promise_results());
+    println!("[BUY] Gas burnt: {} TeraGas", outcome.gas_burnt() as f64 / 1e12);
+    println!("[BUY] Restored storage price : {} Bytes", restored_storage_price_for_buy);
 }
--- tests/utils.rs
+++ tests/utils.rs
@@ -1,5 +1,6 @@
 use paras_marketplace_contract::ContractContract as MarketplaceContract;
 use near_sdk::serde_json::json;
+use near_sdk::json_types::{U128};
 use near_sdk_sim::{
     deploy, init_simulator, to_yocto, ContractAccount, UserAccount, DEFAULT_GAS, STORAGE_AMOUNT,
 };
@@ -12,7 +13,68 @@ near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
 pub const NFT_ID: &str = "nft";
 pub const MARKETPLACE_ID: &str = "marketplace";
 
-pub fn init() -> (ContractAccount<MarketplaceContract>, UserAccount, UserAccount, UserAccount, UserAccount) {
+pub const STORAGE_MINT_ESTIMATE: u128 = 11280000000000000000000;
+pub const STORAGE_CREATE_SERIES_ESTIMATE: u128 = 8540000000000000000000;
+
+// After calculation
+pub const STORAGE_ADD_MARKET_DATA: u128 = 4020000000000000000000;
+
+pub fn create_nft_and_mint_one(
+    nft: &UserAccount, 
+    owner: &UserAccount, 
+    creator: &UserAccount,
+    receiver_id: &UserAccount,
+) {
+    owner.call(
+        nft.account_id(),
+        "nft_create_series",
+        &json!({
+            "token_series_id": u128::MAX.to_string(),
+            "token_metadata": {
+                "title": "A".repeat(200),
+                "reference": "A".repeat(59),
+                "media": "A".repeat(59),
+                "copies": 100u64,
+            },
+            "creator_id": creator.valid_account_id(), 
+            "price": to_yocto("1").to_string(),
+            "royalty": {
+                owner.account_id.clone(): 1000u32,
+                "g".repeat(64): 1000u32,
+                "h".repeat(64): 1000u32,
+                "i".repeat(64): 1000u32,
+                "j".repeat(64): 1000u32,
+                "k".repeat(64): 1000u32,
+                "l".repeat(64): 1000u32,
+                "m".repeat(64): 500u32,
+                "n".repeat(64): 500u32,
+            },
+        }).to_string().into_bytes(),
+        DEFAULT_GAS,
+        STORAGE_CREATE_SERIES_ESTIMATE*2, //royalty 
+    ).assert_success();
+
+
+    receiver_id.call(
+        nft.account_id(),
+        "nft_buy",
+        &json!({
+            "token_series_id": u128::MAX.to_string(),
+            "receiver_id": receiver_id.valid_account_id(),
+        }).to_string().into_bytes(),
+        DEFAULT_GAS,
+        to_yocto("1") + STORAGE_MINT_ESTIMATE
+    ).assert_success();
+}
+pub fn init() -> (
+    ContractAccount<MarketplaceContract>, 
+    UserAccount, 
+    UserAccount, 
+    UserAccount, 
+    UserAccount,
+    UserAccount,
+    UserAccount
+) {
     let root = init_simulator(None);
 
     let treasury = root.create_user(
@@ -20,13 +82,58 @@ pub fn init() -> (ContractAccount<MarketplaceContract>, UserAccount, UserAccount
         to_yocto("100")
     );
 
+    root.create_user(
+        "g".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "h".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "i".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "j".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "k".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "l".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "m".repeat(64),
+        to_yocto("100")
+    );
+
+    root.create_user(
+        "n".repeat(64),
+        to_yocto("100")
+    );
+
     let alice = root.create_user(
-        "alice".to_string(),
+        "x".repeat(64),
         to_yocto("100")
     );
 
     let bob = root.create_user(
-        "bob".to_string(),
+        "y".repeat(64),
+        to_yocto("100")
+    );
+
+    let chandra = root.create_user(
+        "z".repeat(64),
         to_yocto("100")
     );
 
@@ -62,5 +169,5 @@ pub fn init() -> (ContractAccount<MarketplaceContract>, UserAccount, UserAccount
         )
     );
 
-    (marketplace_contract, nft_contract, treasury, alice, bob)
+    (marketplace_contract, nft_contract, treasury, alice, bob, chandra, root)
 }
