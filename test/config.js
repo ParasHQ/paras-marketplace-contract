59f0443069cec8b6bbd1655de091684781eacdc0
EdoWahdana <wahdanaedo@gmail.com>
2022-03-24 15:24:47 +0700

    feat: add test for auction

---
+++ test/config.js
@@ -0,0 +1,34 @@
+const marketplaceContractName = "dev-1648023319907-58748753844114";
+const nftContractName = "paras-token-v1.testnet";
+const ftContractName = "dev-1631277489384-75412609538902";
+const ownerAccountName = "testingdo.testnet";
+
+module.exports = function getConfig(network = "testnet") {
+  let config = {
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    explorerUrl: "https://explorer.testnet.near.org",
+    marketplaceContractName: marketplaceContractName,
+    nftContractName: nftContractName,
+    ftContractName: ftContractName,
+    ownerAccountName: ownerAccountName,
+  };
+
+  switch (network) {
+    case "testnet":
+      config = {
+        ...config,
+        GAS: "300000000000000",
+        gas: "300000000000000",
+        gas_max: "300000000000000",
+        DEFAULT_NEW_ACCOUNT_AMOUNT: "2",
+        DEFAULT_NEW_CONTRACT_AMOUNT: "5",
+        GUESTS_ACCOUNT_SECRET:
+          "7UVfzoKZL4WZGF98C3Ue7tmmA6QamHCiB1Wd5pkxVPAc7j6jf3HXz5Y9cR93Y68BfGDtMLQ9Q29Njw5ZtzGhPxv",
+      };
+  }
+
+  return config;
+};
---
+++ test/test-utils.js
@@ -0,0 +1,254 @@
+const fs = require("fs");
+const BN = require("bn.js");
+const nearAPI = require("near-api-js");
+const {
+  KeyPair,
+  Account,
+  Contract,
+  utils: {
+    format: { parseNearAmount },
+  },
+  connect,
+  keyStores,
+} = nearAPI;
+const getConfig = require("./config");
+const {
+  networkId,
+  marketplaceContractName,
+  nftContractName,
+  ftContractName,
+  ownerAccountName,
+  contractMethods,
+  gas,
+  gas_max,
+  nodeUrl,
+  walletUrl,
+  helperUrl,
+  explorerUrl,
+  DEFAULT_NEW_ACCOUNT_AMOUNT,
+  DEFAULT_NEW_CONTRACT_AMOUNT,
+  GUESTS_ACCOUNT_SECRET,
+} = getConfig("testnet");
+
+const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(
+  `${process.env.HOME}/.near-credentials/`
+);
+
+const near = new nearAPI.Near({
+  deps: {
+    keyStore: keyStore,
+  },
+  networkId: networkId,
+  keyStore: keyStore,
+  nodeUrl: nodeUrl,
+  walletUrl: walletUrl,
+});
+
+const marketplaceContractAccount = new nearAPI.Account(
+  near.connection,
+  marketplaceContractName
+);
+const ownerAccount = new nearAPI.Account(near.connection, ownerAccountName);
+const tokenOwnerAccount = new nearAPI.Account(near.connection, "castleoverlord.testnet");
+const bidderAccount = new nearAPI.Account(
+  near.connection,
+  "bobol.testnet"
+);
+
+marketplaceContractAccount.addAccessKey = (publicKey) =>
+  marketplaceContractAccount.addKey(
+    publicKey,
+    marketplaceContractName,
+    {
+      viewMethods: [
+        "get_market_data",
+        "get_offer",
+        "approved_ft_token_ids",
+        "approved_nft_contract_ids",
+        "get_owner",
+        "get_treasury",
+        "get_supply_by_owner_id",
+        "storage_minimum_balance",
+        "storage_balance_of",
+      ],
+      changeMethods: [
+        "new",
+        "storage_deposit",
+        "storage_withdraw",
+        "add_approved_ft_token_ids",
+        "add_approved_nft_contract_ids",
+        "add_approved_paras_nft_contract_ids",
+        "remove_approved_nft_contract_ids",
+        "buy",
+        "add_offer",
+        "delete_offer",
+        "add_bid",
+        "accept_bid",
+        "update_market_data",
+        "delete_market_data",
+      ],
+    },
+    parseNearAmount("0.1")
+  );
+
+const marketplaceContract = new nearAPI.Contract(
+  marketplaceContractAccount,
+  marketplaceContractName,
+  {
+    viewMethods: [
+      "get_market_data",
+      "get_offer",
+      "approved_ft_token_ids",
+      "approved_nft_contract_ids",
+      "get_owner",
+      "get_treasury",
+      "get_supply_by_owner_id",
+      "storage_minimum_balance",
+      "storage_balance_of",
+    ],
+    changeMethods: [
+      "new",
+      "storage_deposit",
+      "storage_withdraw",
+      "add_approved_ft_token_ids",
+      "add_approved_nft_contract_ids",
+      "add_approved_paras_nft_contract_ids",
+      "remove_approved_nft_contract_ids",
+      "buy",
+      "add_offer",
+      "delete_offer",
+      "add_bid",
+      "accept_bid",
+      "update_market_data",
+      "delete_market_data",
+    ],
+  }
+);
+
+const nftContract = new nearAPI.Contract(ownerAccount, nftContractName, {
+  viewMethods: ["nft_token"],
+  changeMethods: ["nft_transfer", "nft_transfer_call"],
+});
+
+const ftContract = new nearAPI.Contract(ownerAccount, ftContractName, {
+  viewMethods: ["ft_balance_of"],
+  changeMethods: ["ft_transfer", "ft_transfer_call", "storage_deposit"],
+});
+
+async function initContract() {
+  try {
+    await marketplaceContract.new({
+      owner_id: marketplaceContractName,
+    });
+  } catch (e) {
+    throw e;
+  }
+  return { marketplaceContract, marketplaceContractName };
+}
+
+const getAccountBalance = async (accountId) =>
+  new nearAPI.Account(connection, accountId).getAccountBalance();
+
+const initAccount = async (accountId, secret) => {
+  account = new nearAPI.Account(connection, accountId);
+  const newKeyPair = KeyPair.fromString(secret);
+  keyStore.setKey(networkId, accountId, newKeyPair);
+  return account;
+};
+
+const createOrInitAccount = async (
+  accountId,
+  secret = GUESTS_ACCOUNT_SECRET,
+  amount = DEFAULT_NEW_CONTRACT_AMOUNT
+) => {
+  let account;
+  try {
+    account = await createAccount(accountId, amount, secret);
+  } catch (e) {
+    if (!/because it already exists/.test(e.toString())) {
+      throw e;
+    }
+    account = initAccount(accountId, secret);
+  }
+  return account;
+};
+
+async function getAccount(
+  accountId,
+  fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT,
+  secret
+) {
+  accountId = accountId || generateUniqueSubAccount();
+  const account = new nearAPI.Account(connection, accountId);
+  try {
+    await account.state();
+    return account;
+  } catch (e) {
+    if (!/does not exist/.test(e.toString())) {
+      throw e;
+    }
+  }
+  return await createAccount(accountId, fundingAmount, secret);
+}
+
+async function getContract(account) {
+  return new Contract(account || contractAccount, contractName, {
+    ...contractMethods,
+    signer: account || undefined,
+  });
+}
+
+const createAccessKeyAccount = (key) => {
+  connection.signer.keyStore.setKey(networkId, contractName, key);
+  return new Account(connection, contractName);
+};
+
+function generateUniqueSubAccount() {
+  return `t${Date.now()}.${contractName}`;
+}
+
+/// internal
+async function createAccount(
+  accountId,
+  fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT,
+  secret
+) {
+  const contractAccount = new Account(connection, contractName);
+  const newKeyPair = secret
+    ? KeyPair.fromString(secret)
+    : KeyPair.fromRandom("ed25519");
+  await contractAccount.createAccount(
+    accountId,
+    newKeyPair.publicKey,
+    new BN(parseNearAmount(fundingAmount))
+  );
+  keyStore.setKey(networkId, accountId, newKeyPair);
+  return new nearAPI.Account(connection, accountId);
+}
+
+module.exports = {
+  near,
+  gas,
+  gas_max,
+  keyStore,
+  getContract,
+  getAccountBalance,
+  marketplaceContractAccount,
+  marketplaceContractName,
+  nftContractName,
+  ftContractName,
+  ownerAccountName,
+  marketplaceContract,
+  nftContract,
+  ftContract,
+  ownerAccount,
+  contractMethods,
+  tokenOwnerAccount,
+  bidderAccount,
+  initAccount,
+  createOrInitAccount,
+  createAccessKeyAccount,
+  initContract,
+  getAccount,
+  explorerUrl
+};
